fcda7a1aadd2a25f34b92cb69d2b87b69ac66d45
CHMERTL <edrifayoub36@gmail.com>
2024-06-08 18:16:36 +0000

    added transfert + payer recharge + donnation

---
+++ src/main/java/com/ensa/jibi/service/QRCodeService.java
@@ -0,0 +1,28 @@
+package com.ensa.jibi.service;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class QRCodeService {
+
+    public String decodeQRCode(File qrCodeImage) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(qrCodeImage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+            return null;
+        }
+    }
+}
