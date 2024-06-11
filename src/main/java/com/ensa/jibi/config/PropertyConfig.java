b76fc82f19801e97426746bddafbcfae09bc6475
Taha <tahabouataoui5@gmail.com>
2024-06-11 12:40:03 +0100

    commit  5

--- src/main/java/com/ensa/jibi/config/PropertyConfig.java
+++ src/main/java/com/ensa/jibi/config/PropertyConfig.java
@@ -1,12 +1,9 @@
 package com.ensa.jibi.config;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("/etc/secrets/secret")
 public class PropertyConfig {
-
-
 }
